0a49f75f0bfc81b0e6d21e9ebde2020d352d94be
Juan Sedano <jsedano.dev@gmail.com>
2024-06-29 22:49:24 -0600

    initial commit

---
+++ src/main/java/dev/jsedano/ezcraft/EzcraftApplication.java
@@ -0,0 +1,13 @@
+package dev.jsedano.ezcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzcraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzcraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jsedano/ezcraft/EzcraftApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jsedano.ezcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzcraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
